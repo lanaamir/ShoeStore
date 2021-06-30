b0aac7d3dc7a153d03cc04c3192fa323a8446b00
chocolatepiedeveloper <52275049+chocolatepiedeveloper@users.noreply.github.com>
2021-06-30 15:27:22 +0300

    Add navigation resources

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,25 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navController = this.findNavController((R.id.nav_host_fragment))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
---
+++ starter/app/src/main/java/com/udacity/shoestore/models/User.kt
@@ -0,0 +1,7 @@
+package com.udacity.shoestore.models
+
+data class User(
+    val name: String = "",
+    val email: String = "",
+    val password: String = ""
+)
